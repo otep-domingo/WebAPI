0f960600c9623d5c7fc3aabe1bcc4dd098318e8a
Prof Prof <domingo.joseph@outlook.ph>
2024-05-28 19:03:24 +0800

    Add project files.

---
+++ WebAPI/Data/DbContextClass.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Model;
+namespace WebAPI.Data
+{
+    public class DbContextClass:DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
